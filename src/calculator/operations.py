5e7271550537efcdb58cd4fe467a90f7a9e60fcd
sushilicp <sushil.parajuli@icp.edu.np>
2025-07-07 18:54:20 +0545

    multiple added

--- src/calculator/operations.py
+++ src/calculator/operations.py
@@ -1,5 +1,10 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
+
+
+def multiple(a, b):
+    return a * b
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,8 +1,15 @@
 from calculator.operations import add
 from calculator.operations import subtract
+from calculator.operations import multiple
+
 
 def test_add():
     assert add(4, 5) == 9
 
+
 def test_subtract():
     assert subtract(4, 5) == -1
+
+
+def test_multiple():
+    assert multiple(4,5) == 20
